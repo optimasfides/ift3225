2f6f117fee3dfd98fc3a7d02716881e5bd0d51eb
optimasfides <optimaa-fide@mail.ru>
2020-07-30 16:09:04 -0400

    Updates

---
+++ precache-manifest.6cbfe720faf226831f6eb64575a99b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775c208a4f18a144d7f6954ecc2381b6",
+    "url": "/ift3225/index.html"
+  },
+  {
+    "revision": "aff17b9ea217e0534e05",
+    "url": "/ift3225/static/css/main.2ed79484.chunk.css"
+  },
+  {
+    "revision": "4cd27cca1022b44c7757",
+    "url": "/ift3225/static/js/2.25f701f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ift3225/static/js/2.25f701f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff17b9ea217e0534e05",
+    "url": "/ift3225/static/js/main.127ab0af.chunk.js"
+  },
+  {
+    "revision": "08448b59b7fb032dfd71",
+    "url": "/ift3225/static/js/runtime-main.21841b8f.js"
+  }
+]);
